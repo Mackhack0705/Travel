3815c6643ae3688ae640d69c8ac64df7265e737c
mackhack0705 <malirajesh0705@gmail.com>
2026-05-09 21:55:11 +0530

    Updated the customer section for all devices

--- frontend/src/components/Customers.jsx
+++ frontend/src/components/Customers.jsx
@@ -4,20 +4,22 @@ import "../style/customers.css"
 const Customers = () => {
   return (
     <div className='customer-table'>
-      <table cellSpacing={50} className='a'>
-            <tr className='customer-row1'>
-                <th>9,313</th>
-                <th>8,492</th>
-                <th>100</th>
-                <th>120</th>
-            </tr>
-            <tr className='customer-row2'>
-                <td>No. of Travels</td>
-                <td>No. of Clients</td>
-                <td>No. of Employees</td>
-                <td>No. of Countries</td>
-            </tr>
-      </table>
+      <div className='fl table-items'>
+        <p>9,313</p>
+        <p>No. of Travels</p>
+      </div>
+      <div className='fl table-items'>
+        <p>8,492</p>
+        <p>No. of Clients</p>
+      </div>
+      <div className='fl table-items'>
+        <p>100</p>
+        <p>No. of Employees</p>
+      </div>
+      <div className='fl table-items'>
+        <p>120</p>
+        <p>No. of Countries</p>
+      </div>
     </div>
   )
 }
