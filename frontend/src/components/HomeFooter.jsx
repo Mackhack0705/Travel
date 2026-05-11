2c852540592b5611b84b0bfdcb94f750c9a34164
mackhack0705 <malirajesh0705@gmail.com>
2026-05-11 23:05:43 +0530

    Updated the footer section for mobile view

--- frontend/src/components/HomeFooter.jsx
+++ frontend/src/components/HomeFooter.jsx
@@ -1,46 +1,48 @@
 import React from 'react'
-import { FaFacebook } from 'react-icons/fa'
-import { FaInstagram } from 'react-icons/fa'
-import { FaTwitter } from 'react-icons/fa'
+import { FaFacebook, FaLinkedinIn, FaInstagram, FaTwitter } from 'react-icons/fa'
 import logo from '../images/Company-logo.png'
 import '../style/homefooter.css'
 
 const HomeFooter = () => {
   return (
     <div className='footer-section'>
-      <table className='footer-table'> 
-        <tr>
-            <th><img src={logo} alt="" style={{height: 50, width: 80}} /></th>
-            <th>Company</th>
-            <th>Contact</th>
-            <th>More</th>
-            <th><FaFacebook className='contact-logo' size={25}/>
-            <FaInstagram className='contact-logo' size={25}/>
-            <FaTwitter className='contact-logo' size={25}/> </th>
-            
-        </tr>
-        <tr>
-            <td rowSpan={3}>Book your trip in minute, get full control for much longer.</td>
-            <td><a href="#About">About</a></td>
-            <td><a href="#FAQ">Help/FAQ</a></td>
-            <td><a href="#AirlineFees">Airlinefees</a></td>
-            <td>Discover our app</td>
-        </tr>
-        <tr>
-            <td><a href="#Career">Careers</a></td>
-            <td><a href="#Press">Press</a></td>
-            <td><a href="#Airline">Airline</a></td>
-            <td rowSpan={2} >
-              <img src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png" alt="" />
-              <img src="https://w7.pngwing.com/pngs/1015/380/png-transparent-app-store-logo-iphone-app-store-google-play-apple-app-store-electronics-text-logo.png" alt="" />
-            </td>
-        </tr>
-        <tr>
-            <td><a href="#Mobile">Mobile</a></td>
-            <td><a href="#Affiliate">Affiliate</a></td>
-            <td><a href="#tips">Low far tips</a></td>
-        </tr>
-      </table>
+      <div className='brand-section'>
+        <div className='logo-section'>
+          <img src={logo} alt="Company Logo" className='logo-img'/>
+        </div>
+        <div className='fl contact-section'>
+          <FaInstagram className='contact-logo' size={40}/>
+          <FaLinkedinIn className='contact-logo' size={40}/>
+          <FaTwitter className='contact-logo' size={40}/>
+          <FaFacebook className='contact-logo' size={40}/>
+        </div>
+        <div className='fl app-section'>
+          <img src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png" alt="" className='store'/>
+          <img width="280" height="100" src="https://w7.pngwing.com/pngs/1015/380/png-transparent-app-store-logo-iphone-app-store-google-play-apple-app-store-electronics-text-logo.png" alt="" className=''/>
+        </div>
+      </div>
+      <div className='links-section'>
+        <div>
+            <p className='link-heading'>Company</p>
+            <p><a href="#About">About</a></p>
+            <p><a href="#FAQ">Help/FAQ</a></p>
+            <p><a href="#AirlineFees">Airlinefees</a></p>
+        </div>
+        <div>
+        </div>
+        <div>
+            <p className='link-heading'>Contact</p>
+            <p><a href="#Career">Careers</a></p>
+            <p><a href="#Press">Press</a></p>
+            <p><a href="#Airline">Airline</a></p>
+        </div>
+        <div>
+            <p className='link-heading'>More</p>
+            <p><a href="#Mobile">Mobile</a></p>
+            <p><a href="#Affiliate">Affiliate</a></p>
+            <p><a href="#tips">Low far tips</a></p>
+        </div>
+      </div>
     </div>
   )
 }
